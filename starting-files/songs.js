e6d14f46ac5679c953c43824fdabe4b22fb0e974
vannex2121 <119449316+vannex2121@users.noreply.github.com>
2023-11-20 18:58:28 +0000

    changes for the milestone2

--- starting-files/songs.js
+++ starting-files/songs.js
@@ -16,32 +16,14 @@ function populateSelectOptions(selectElement, dataArray) {
         selectElement.add(option);
     });
 }
-
 populateSelectOptions(artistSelect, artists);
 populateSelectOptions(genreSelect, genres);
-
-// Function to populate the song list
-function populateSongList() {
-    songListBody.innerHTML = "";
-
-    songs.forEach(song => {
-        const { title, artist, year, genre, details } = song;
-        const row = songListBody.insertRow();
-
-        row.insertCell(0).innerText = title;
-        row.insertCell(1).innerText = findArtistName(artist.id);
-        row.insertCell(2).innerText = year;
-        row.insertCell(3).innerText = findGenreName(genre.id);
-        row.insertCell(4).innerText = details.popularity;
-    });
-}
 
 // Function to find name by ID
 function findNameById(id, dataArray) {
     const item = dataArray.find(item => item.id === id);
     return item ? item.name : "Unknown";
 }
-
 // Function to find artist name by artist ID
 function findArtistName(artistId) {
     return findNameById(artistId, artists);
@@ -52,13 +34,13 @@ function findGenreName(genreId) {
 }
 // Function to populate the song list
 function populateSongList() {
-    const songListBody = document.getElementById("songTableBody");
+const songListBody = document.getElementById("songTableBody");
     songListBody.innerHTML = "";
 
-    // Sort the songs array alphabetically by title
+// Sort the songs array alphabetically by title
     songs.sort((a, b) => a.title.localeCompare(b.title, undefined, { sensitivity: 'base' }));
 
-    // Populate the sorted song list using a for loop
+// Populate the sorted song list using a for loop
     for (let i = 0; i < songs.length; i++) {
         const song = songs[i];
         const row = songListBody.insertRow();
@@ -68,11 +50,10 @@ function populateSongList() {
         row.insertCell(3).innerText = findGenreName(song.genre.id);
         row.insertCell(4).innerText = song.details.popularity;
 
-        // Set data attributes for artist and genre IDs
+    // Set data attributes for artist and genre IDs
         row.setAttribute("data-artist-id", song.artist.id);
         row.setAttribute("data-genre-id", song.genre.id);
     }
 }
-
 // Initial population of the song list
 populateSongList();
