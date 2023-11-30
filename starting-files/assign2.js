05ab1c86f7eecabd0c77f887385746780e60b74d
Juve Lopena <119449316+vannex2121@users.noreply.github.com>
2023-11-30 08:18:50 -0700

    Update assign2.js

--- starting-files/assign2.js
+++ starting-files/assign2.js
@@ -1,8 +1,7 @@
 
 
-
 /* url of song api --- https versions hopefully a little later this semester */	
-const api = 'http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php';
+const api = 'https://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php';
 
  
 
@@ -11,3 +10,20 @@ const api = 'http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.p
    Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
    use built-in Live Preview.
 */
+async function getApiData(api) {
+   try {
+     const response = await fetch(api);
+     const data = await response.json();
+ 
+     const genreList1 = data.map(song => song.genre);
+     const artistList1 = data.map(song => song.artist);
+     const songData1 = data;
+ 
+     return { genreList1, artistList1, songData1 };
+   } catch (error) {
+     console.error("Error fetching API data:", error);
+     return { error: "Error fetching API data" };
+   }
+ }
+ export { getApiData, api };
+ 
